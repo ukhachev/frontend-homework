127de236b7d104eda4a9072f99779d9839e222b8
Vladimir <ukhachev.v.s@gmail.com>
2019-02-12 13:23:49 +0300

    Add tests

--- tests/format.js
+++ tests/format.js
@@ -51,4 +51,24 @@ QUnit.module('Тестируем функцию format', function () {
 		assert.strictEqual(format(input, 2), expected2);
 		assert.strictEqual(format(input, 3), expected3);
 	});
+	QUnit.test('format работает правильно c одним числом', function (assert) {
+		const input = [ 1000 ];
+
+		const expected =
+			'1000';
+
+		assert.strictEqual(format(input, 1), expected);
+	});
+	QUnit.test('format работает правильно c одной строкой и несколькими столбцами', function (assert) {
+		const input1 = [ 1, 2, 1000 ];
+
+		const expected1 =
+			'1 2 1000';
+		const input2 = [ 234, 12000 ];
+
+		const expected2 =
+			'234 12000';
+		assert.strictEqual(format(input1, 3), expected1);
+		assert.strictEqual(format(input2, 2), expected2);
+	});
 });
